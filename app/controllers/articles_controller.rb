29971e44eff960da272b5b24aabfbac7268dcfd8
Abdullah Numan <anewman15@hotmail.com>
2020-09-03 19:04:47 +0600

    Add edit, update actions and then their views

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -20,6 +20,17 @@ class ArticlesController < ApplicationController
     redirect_to article_path(@article)
   end
 
+  def edit
+    @article = Article.find(params[:id])
+  end
+
+  def update
+    @article = Article.find(params[:id])
+    @article.update(article_params)
+  
+    redirect_to article_path(@article)
+  end
+
   def destroy
     @article = Article.find(params[:id]).destroy
     redirect_to articles_path
